9ff50d7f5b7cb75246f05b721af6ecbd5370b928
TuNombreDeUsuario <tuemail@ejemplo.com>
2025-09-12 14:36:49 -0500

    added: now we can ignore _ variables

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,19 @@ export default tseslint.config([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      // Sobrescribe la regla para ignorar vars/args con prefijo _ (como en TS nativo)
+      '@typescript-eslint/no-unused-vars': [
+        'error',  // Mantiene el nivel de error (no la desactivas)
+        {
+          args: 'all',  // Chequea todos los args
+          argsIgnorePattern: '^_',  // Ignora args que empiecen con _
+          varsIgnorePattern: '^_',  // Ignora variables que empiecen con _
+          caughtErrorsIgnorePattern: '^_',  // Ignora errores en catch con _
+          destructuredArrayIgnorePattern: '^_',  // Ignora en destructuring de arrays
+          ignoreRestSiblings: true,  // Ignora hermanos en rest params
+        },
+      ],
+    },
   },
 ])
